fde37a24c5af44a6293e20963b0a69fcf260ccbd
GuiLuv <luvielmo.guilherme@gmail.com>
2020-03-28 17:38:22 -0300

    first commit

---
+++ ConsumeWhoisAPIv2/Models/DomainModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWhoisAPIv2.Models
+{
+    public class DomainModel
+    {
+        public int Id { get; set; }
+
+        // Claro
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string RegisterDate { get; set; }
+        public string LastUpdate { get; set; }
+        public string Expires { get; set; }
+        public string[] NameServers { get; set; }   
+
+    }
+}
---
+++ ConsumeWhoisAPIv2/Models/RequestApi.cs
@@ -0,0 +1,14 @@
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumeWhoisAPIv2.Models
+{
+    public class RequestApi
+    {
+        private readonly RestClient client;
+
+    }
+}
